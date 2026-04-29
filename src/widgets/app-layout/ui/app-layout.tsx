f4d7d8b7c152f9478c5839da90990148a0a3ab0e
Denis <den_71293@mail.ru>
2026-04-29 19:11:47 +0300

    add sonner in a project

--- src/widgets/app-layout/ui/app-layout.tsx
+++ src/widgets/app-layout/ui/app-layout.tsx
@@ -3,6 +3,7 @@ import { SidebarInset } from '@/shared/ui/shadcn/sidebar';
 import { AppHeader } from '@/widgets/app-header/ui/app-header';
 import { AppSidebar } from '@/widgets/app-sidebar/ui/app-sidebar';
 import { AppBody } from '@/widgets/app-body/ui/app-body';
+import { Toaster } from '@/shared/ui/shadcn/sonner';
 
 export const AppLayout = () => {
 	return (
@@ -11,6 +12,7 @@ export const AppLayout = () => {
 			<SidebarInset>
 				<AppHeader />
 				<AppBody />
+				<Toaster position="top-center" />
 			</SidebarInset>
 		</AppProvider>
 	);
