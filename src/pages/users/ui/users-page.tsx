96c309773b7ee378239959b821255d9d9d640a75
Denis <den_71293@mail.ru>
2026-05-06 16:37:08 +0300

    Страница пользователей

--- src/pages/users/ui/users-page.tsx
+++ src/pages/users/ui/users-page.tsx
@@ -1,3 +1,40 @@
+import { getUsers } from '@/features/users/api/get-users';
+import type { User } from '@/features/users/model/type';
+import { columns } from '@/features/users/ui/users-column';
+import { DataTable } from '@/shared/ui/data-table';
+import { useCallback, useEffect, useState } from 'react';
+
 export const UsersPage = () => {
-	return <div>Пользователи</div>;
+	const [users, setUsers] = useState<User[]>([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState<Error | null>(null);
+
+	const fetchUsers = useCallback(async () => {
+		try {
+			setIsLoading(true);
+			setError(null);
+			const data = await getUsers();
+			setUsers(data);
+		} catch (error) {
+			setError(error instanceof Error ? error : new Error('Произошла ошибка при загрузке пользователей'));
+		} finally {
+			setIsLoading(false);
+		}
+	}, []);
+
+	useEffect(() => {
+		const ac = new AbortController();
+
+		void fetchUsers();
+
+		return () => {
+			ac.abort();
+		};
+	}, []);
+
+	return (
+		<div>
+			<DataTable data={users} isLoading={isLoading} error={error} columns={columns} />
+		</div>
+	);
 };
