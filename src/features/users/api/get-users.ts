e2e7f8704c25cf30a643c0d41ae8f0277feda2fc
Denis <den_71293@mail.ru>
2026-05-06 16:36:58 +0300

    Получение пользователей из БД

---
+++ src/features/users/api/get-users.ts
@@ -0,0 +1,14 @@
+import { imageRetouchApi } from '@/shared/api/client/instance';
+import type { User } from '../model/type';
+
+export const getUsers = async (): Promise<User[]> => {
+	const response = await imageRetouchApi.get('/users');
+
+	if (response.status == 401) {
+		throw new Error('Необходимо авторизоваться');
+	}
+
+	const data = response.data as User[];
+
+	return data;
+};
