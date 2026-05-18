7d919ba4547e82fd877c54794a9c48cc1dbe7ce6
Denis <den_71293@mail.ru>
2026-05-18 15:48:28 +0300

    Удалить заказ

--- src/features/tasks/api/delete-order.ts
+++ src/features/tasks/api/delete-order.ts
@@ -1,7 +1,7 @@
 import { imageRetouchApi } from '@/shared/api/client/instance';
 
 export const deleteOrder = async (id: string): Promise<void> => {
-	const response = await imageRetouchApi.delete(`/orders/archiveOrder/${id}`);
+	const response = await imageRetouchApi.delete(`/orders/deleteOrder/${id}`);
 
 	if (response.status === 401) {
 		throw new Error('Необходимо авторизоваться');
